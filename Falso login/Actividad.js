ef8eec82c7b81a29d4995586b6b2057d0cbbb1b7
luanalopez20 <luanaslopez@outlook.com>
2025-06-02 15:56:45 -0300

    Falso login

---
+++ Falso login/Actividad.js
@@ -0,0 +1,7 @@
+const usuario = "Luana";
+const Contraseña = "1234";
+
+function verificarLogin() {
+  const usuarioLogin = document.getElementById("usuario").value;
+  const contraseñaLogin = document.getElementById("Contraseña").value;
+}
--- javascript/manipulacion-dom/Ejercicio2.js
+++ javascript/manipulacion-dom/Ejercicio2.js
@@ -1,54 +1,31 @@
-
-
-
-
 let lista = [];
 
+function agregarUnaTarea() {
+  const input = document.getElementById("input-task");
+  const text = input.value;
+  console.log(text);
 
-function agregarUnaTarea(){
-const input= document.getElementById("input-task");
-const text = input.value;
-console.log(text);
+  const obj = { task: text, completed: false };
+  if (text !== "") {
+    lista.push(obj);
+  }
 
+  const list = document.getElementById("listaTareas");
 
+  //crear elemento html
 
+  const li = document.createElement("li");
+  li.textContent = obj.task;
+  //
+  const botonDelete = document.createElement("button");
+  botonDelete.textContent = "Delete";
 
-const obj =  {task:text, completed:false};
-if (text !==""){
-   lista.push(obj);
- }
+  list.append(li, botonDelete);
 
-
-
-
-
-
-
-
-const list = document.getElementById("listaTareas")
-
-
-
-
-//crear elemento html
-
-
-const li = document.createElement("li")
-li.textContent= obj.task;
-//
-const botonDelete=document.createElement("button")
-botonDelete.textContent = "Delete"
-
-
-list.append(li, botonDelete);
-
-
-//esta mal hecho hacer la clase que viene //
-if (obj.completed ===true){
-   botonDelete.onclick(()=>{
-       li.removeChild
-   })
+  //esta mal hecho hacer la clase que viene //
+  if (obj.completed === true) {
+    botonDelete.onclick(() => {
+      li.removeChild;
+    });
+  }
 }
-
-}
-
