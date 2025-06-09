3a1efdd4d084f7e5c1b7806810c6cdcda12fe574
luanalopez20 <luanaslopez@outlook.com>
2025-06-09 16:38:22 -0300

    Falso login sin terminar

--- Falso login/Actividad.js
+++ Falso login/Actividad.js
@@ -1,7 +1,38 @@
-const usuario = "Luana";
-const Contraseña = "1234";
+const Contraseña = "lopez";
+const Usuario = "Luanals";
+async function obtenerDatos() {
+ try {
+   let response = await fetch("https://jsonplaceholder.typicode.com/users");
+   let data = await response.json();
+   console.log(data);
+ } catch (error) {
+   console.error("Error:", error);
+ }
+}
+
+
+obtenerDatos();
+
 
 function verificarLogin() {
-  const usuarioLogin = document.getElementById("usuario").value;
-  const contraseñaLogin = document.getElementById("Contraseña").value;
+ const usuarioInput = document.getElementById("Usuario").value;
+ const contraseñaInput = document.getElementById("Contraseña").value;
+
+
+ if (Usuario === usuarioInput && Contraseña === contraseñaInput) {
+   alert("¡Bienvenido, " + Usuario + "!");
+ } else {
+   error.textContent = "Usuario o contraseña incorrectos.";
+   return;
+ }
+
+
+ if (Usuario === usuarioInput && Contraseña === contraseñaInput) {
+   localStorage.setItem("sesion", usuarioInput);
+ } else {
+   error.textContent = "Datos incorrectos";
+ }
+
+ obtenerDatos();
 }
+
